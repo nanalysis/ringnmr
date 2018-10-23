23156bcec586de41d94fa49ba61d74fd85b69cc4
Martha Beckwith <mbeckwith@guanine.asrc.cuny.edu>
2018-10-23 16:19:26 -0400

    Add CEST Equations in Preferences GUI.

--- src/main/java/org/comdnmr/fit/calc/CoMDPreferences.java
+++ src/main/java/org/comdnmr/fit/calc/CoMDPreferences.java
@@ -5,6 +5,10 @@
  */
 package org.comdnmr.fit.calc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.prefs.Preferences;
 
 /**
@@ -16,6 +20,7 @@ public class CoMDPreferences {
     static private Double cpmgMaxFreq = null;
     static private Double rexRatio = null;
     static private Integer sampleSize = null;
+    private static Map<String, Boolean> eqnMap = new HashMap<>();
 
     static Preferences getPrefs() {
         Preferences prefs = Preferences.userNodeForPackage(CoMDPreferences.class);
@@ -71,6 +76,35 @@ public class CoMDPreferences {
         } else {
             getPrefs().remove("REX_RATIO");
         }
+    }
+    
+    public static void setEqnMap(Map<String, Boolean> map) {
+        eqnMap = map;
+    }
+    public static void saveEqnPrefs() {
+        Preferences prefs = Preferences.userNodeForPackage(ExperimentData.class);
+        StringBuilder sBuilder = new StringBuilder();
+        for (String eqn : eqnMap.keySet()){
+            sBuilder.append(eqn);
+            sBuilder.append(';');
+            sBuilder.append(eqnMap.get(eqn));
+            sBuilder.append("\n");
+        }
+        prefs.put("CESTEQNS", sBuilder.toString());
+    }
+
+    public static List<String> getEqns() {
+        Preferences prefs = Preferences.userNodeForPackage(ExperimentData.class);
+        String eqns = prefs.get("CESTEQNS", null);
+        String[] cestEqnPrefList = eqns.split("\n");
+        List<String> cestEqnList = new ArrayList<>();
+        for (String cestEqnPref : cestEqnPrefList) {
+            String[] cestEqnPrefs = cestEqnPref.split(";");
+            if (cestEqnPrefs[1].equals("true")) {
+                cestEqnList.add(cestEqnPrefs[0]);
+            }
+        }
+        return cestEqnList;
     }
 
 }
