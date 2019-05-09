ccef4e749be710bf9b38810da0a426927601d14d
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2019-05-09 12:36:11 -0400

    Increase range for dppmMin

--- src/main/java/org/comdnmr/fit/gui/CPMGControls.java
+++ src/main/java/org/comdnmr/fit/gui/CPMGControls.java
@@ -40,7 +40,7 @@ public class CPMGControls extends EquationControls {
     enum PARS implements ParControls {
         R2("R2", 0.0, 50.0, 10.0, 10.0, 2),
         KEX("Kex", 0.0, 4000.0, 500.0, 500.0, 1),
-        DPPMMIN("dPPMmin", 0.0, 0.2, 0.05, 0.05, 3),
+        DPPMMIN("dPPMmin", 0.0, 2.0, 0.2, 0.2, 3),
         PA("pA", 0.5, 0.99, 0.1, 0.9, 3),
         DPPM("dPPM", 0.0, 2.0, 0.2, 0.2, 3),
         FIELD2("Field2", 500.0, 1200.0, 100.0, 600.0, 1);
