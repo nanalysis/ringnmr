b1b3a4b29843d3a8af92c8e0a1e2afa3616ef21b
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2019-07-25 16:42:17 -0400

    Improve RelaxEquations

--- src/main/java/org/comdnmr/fit/calc/RelaxEquations.java
+++ src/main/java/org/comdnmr/fit/calc/RelaxEquations.java
@@ -1,21 +1,56 @@
 package org.comdnmr.fit.calc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author brucejohnson
  */
 public class RelaxEquations {
 
-    double mu0 = 4.0e-7 * Math.PI;
-    double gammaI = -2.71e7;
-    double gammaS = 2.68e8;
-    double planck = 1.054e-34;
-    double r = 1.01e-10;
+    public final static double MU0 = 4.0e-7 * Math.PI;
+    public final static double GAMMA_N = -2.71e7;
+    public final static double GAMMA_H = 2.68e8;
+    public final static double PLANCK = 1.054e-34;
+    public final static double R_HN = 1.01e-10;
+    public final static double SIGMA = 165.0e-6;
+    public final static Map<String, Double> GAMMA_MAP = new HashMap<>();
+    public final static Map<String, Double> R_MAP = new HashMap<>();
+
+    static {
+        GAMMA_MAP.put("H", GAMMA_H);
+        GAMMA_MAP.put("N", GAMMA_N);
+        R_MAP.put("HN", R_HN);
+        R_MAP.put("NH", R_HN);
+    }
+
+    private final double r;
+    private final double d;
+    private final double d2;
+    private final double c;
+    private final double c2;
+    private final double gammaS;
+    private final double gammaI;
+    private final double sf;
+    private final double wI;
+    private final double wS;
+
     // don't use this yet.  Various inconsistencies with various different presentations of equations
     //   consider using scaled versions (smaller exponents)
+    // add csa
+    public RelaxEquations(double sf, String elem1, String elem2) {
+        gammaI = GAMMA_MAP.get(elem1);
+        gammaS = GAMMA_MAP.get(elem2);
+        wI = sf * 2.0 * Math.PI;
+        wS =wI * gammaS / gammaI;
+        r = R_MAP.get(elem1 + elem2);
+        d = MU0 * (gammaI * gammaS * PLANCK) / (4.0 * Math.PI * r * r * r);
+        d2 = d * d;
+        c = wS * SIGMA / Math.sqrt(3.0);
+        c2 = c * c;
 
-    public RelaxEquations() {
-
+        this.sf = sf;
     }
 
     public double JModelFree(double w, double tau, double taui, double s2) {
@@ -26,38 +61,31 @@ public class RelaxEquations {
     }
 
     public double J(double w, double tau) {
-        double value = tau / (1.0 + w * w * tau * tau);
+        double value = 0.4 * tau / (1.0 + w * w * tau * tau);
         return value;
     }
 
-    public double T1(double tau) {
-        double wH = 800.0e6 * 2.0 * Math.PI;
-        double wN = Math.abs(wH * gammaI / gammaS);
-        double A = (J(wH - wN, tau) + 3.0 * J(wN, tau) + 6.0 * J(wH + wN, tau));
-        double d = mu0 * (gammaS * gammaI * planck) / (4.0 * Math.PI * r * r * r);
-        double d2 = 0.1 * d * d;
+    public double r2r1Ratio(double tau) {
+        double num = 4.0 * J(0, tau) + J(wS - wI, tau) + 3.0 * J(wS, tau)
+                + 6.0 * J(wI, tau) + 6.0 * J(wS + wI, tau)
+                + (c2 / (3.0 * d2) * (4.0 * J(0, tau) + 3.0 * J(wS, tau)));
 
-        double invT1 = d2 * A;
-        return invT1;
+        double denom = 2.0 * J(wS - wI, tau) + 6.0 * J(wS, tau) + 12.0 * J(wS + wI, tau) + 2.0 * (c2 / (3.0 * d2) * J(wS, tau));
+        return num / denom;
+    }
+
+    public double R1(double tau) {
+        double dipolarContrib = d2 / 4.0 * (J(wI - wS, tau) + 3.0 * J(wS, tau) + 6.0 * J(wI + wS, tau));
+        double csaContrib = c2 * J(wS, tau);
+        return dipolarContrib + csaContrib;
 
     }
 
-    public double T2(double tau) {
-        double wH = 800.0e6 * 2.0 * Math.PI;
-        double wN = Math.abs(wH * gammaI / gammaS);
-        double A = (4.0 * J(0.0, tau) + J(wH - wN, tau) + 3.0 * J(wN, tau) + 6.0 * J(wH, tau) + 6.0 * J(wH + wN, tau));
-        double d = mu0 * (gammaS * gammaI * planck) / (4.0 * Math.PI * r * r * r);
-        double d2 = 0.1 * d * d;
-
-        double invT1 = 0.5 * d2 * A;
-        return invT1;
-
+    public double R2(double tau) {
+        double dipolarContrib = d2 / 8.0 * (4.0 * J(0.0, tau) + J(wI - wS, tau) + 3.0 * J(wS, tau)
+                + 6.0 * J(wI, tau) + 6.0 * J(wI + wS, tau));
+        double csaContrib = c2 / 6 * (4.0 * J(0.0, tau) + 3.0 * J(wS, tau));
+        return dipolarContrib + csaContrib;
     }
 
-    public double T2s(double tau) {
-        double A = (1.0 / Math.pow((2.0 * Math.PI), 2)) * Math.pow(mu0 / (4.0 * Math.PI), 2);
-        double x = Math.pow(gammaI * gammaS * planck, 2) / Math.pow(r, 6);
-        double invT2 = A * x * tau;
-        return invT2;
-    }
 }
