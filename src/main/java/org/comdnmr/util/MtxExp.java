8bdaf439a257d52aef6b160372c37f98d4055fbd
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2019-11-20 15:59:58 -0500

    Format

--- src/main/java/org/comdnmr/util/MtxExp.java
+++ src/main/java/org/comdnmr/util/MtxExp.java
@@ -14,8 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-/*
+ */
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -30,14 +30,15 @@ import org.apache.commons.math3.linear.LUDecomposition;
 /**
  *
  * @author Martha Beckwith
- * 
+ *
  * computes the exponential of a matrix, M: exp[M]
- * 
- * based on the Matlab function c8mat_expm1() from https://people.sc.fsu.edu/~jburkardt/m_src/matrix_exponential/r8mat_expm1.m
- * 
+ *
+ * based on the Matlab function c8mat_expm1() from
+ * https://people.sc.fsu.edu/~jburkardt/m_src/matrix_exponential/r8mat_expm1.m
+ *
  */
 public class MtxExp {
-    
+
     public static double[][] matrixExp(double[][] A1) {
         RealMatrix A = new Array2DRowRealMatrix(A1);
         RealMatrix E = matrixExp(A);
@@ -54,22 +55,22 @@ public class MtxExp {
         //RealMatrix A = A1.copy();
         int e = (int) (Math.log(A.getNorm()) / Math.log(2));  // log base 2 of the infinity norm of A.
         int s = Math.max(0, e + 1);
-      
+
         A = A.scalarMultiply(1 / Math.pow(2, s));
-        
+
         RealMatrix X = A.copy();
         double c = 0.5;
         RealMatrix E = MatrixUtils.createRealIdentityMatrix(A.getRowDimension()).add(A.scalarMultiply(c));
         RealMatrix D = MatrixUtils.createRealIdentityMatrix(A.getRowDimension()).subtract(A.scalarMultiply(c));
         double q = 6;
         boolean p = true;
-        
-        for(int k = 2; k <= q; k++) {
+
+        for (int k = 2; k <= q; k++) {
             c = c * (q - k + 1) / (k * (2 * q - k + 1));
-            X = A.multiply(X);  
+            X = A.multiply(X);
             RealMatrix cX = X.scalarMultiply(c);
             E = E.add(cX);
-          
+
             if (p == true) {
                 D = D.add(cX);
             } else {
@@ -77,11 +78,11 @@ public class MtxExp {
             }
             p = !p;
         }
-        
+
         LUDecomposition LUD = new LUDecomposition(D);
         E = LUD.getSolver().solve(E);
-        
-        for(int k = 1; k <= s; k++) {
+
+        for (int k = 1; k <= s; k++) {
             E = E.multiply(E);
         }
         return E;
