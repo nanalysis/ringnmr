9b6129183398670410d25f3a720c23c0d1190081
Bruce Johnson <bruce@onemoonscientific.com>
2022-03-07 10:43:46 -0500

    Calc reduced J from R1/R2/NOE

--- ringnmr/src/main/java/org/comdnmr/modelfree/MolDataValues.java
+++ ringnmr/src/main/java/org/comdnmr/modelfree/MolDataValues.java
@@ -5,12 +5,13 @@
  */
 package org.comdnmr.modelfree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import org.comdnmr.data.DynamicsSource;
 import org.comdnmr.modelfree.models.MFModel;
 import org.nmrfx.chemistry.Atom;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  *
@@ -23,11 +24,12 @@ public class MolDataValues {
     final double[] vector = new double[3];
     final List<RelaxDataValue> dataValues = new ArrayList<>();
     MFModel model;
+    double[][] jValues = null;
 
     public MolDataValues(String specifier, double[] vector, DynamicsSource dynSourceFactory) {
         this.specifier = specifier;
         Optional<Atom> atomOpt = dynSourceFactory.getAtom(null, specifier, 0, false);
-        atom = atomOpt.get();
+        atom = atomOpt.orElse(null);
         System.arraycopy(vector, 0, this.vector, 0, 3);
     }
 
@@ -72,4 +74,58 @@ public class MolDataValues {
         return vector;
     }
 
+    public double[][] calcJ() {
+        int nDataValues = dataValues.size();
+        int nFreq = 1 + 2 * nDataValues;
+        double[][] result = new double[3][nFreq];
+        int iField = 0;
+        for (RelaxDataValue relaxDataValue: dataValues) {
+            RelaxEquations relaxEq = relaxDataValue.relaxObj;
+            double field = relaxEq.getSF();
+            double r1 = relaxDataValue.R1;
+            double r2 = relaxDataValue.R2;
+            double noe = relaxDataValue.NOE;
+            double r1Err = relaxDataValue.R1err;
+            double r2Err = relaxDataValue.R2err;
+            double noeErr = relaxDataValue.NOEerr;
+            double sigma = (noe - 1.0) * r1 * RelaxEquations.GAMMA_N / RelaxEquations.GAMMA_H;
+            double sigmaErr = sigma * Math.sqrt(Math.pow((noeErr / (noe - 1.0)), 2) + Math.pow((r1Err / r1), 2));
+
+            double d2 = relaxEq.getD2();
+            double c2 = relaxEq.getC2();
+
+            double j87H = 4.0 * sigma / (5.0 * d2);
+            double j87Herr = 4.0 * sigmaErr / (5.0 * d2);
+
+            double jNMul = 4.0 / (3.0 * d2 + 4.0 * c2);
+            double jN = (r1 - 1.249 * sigma) * jNMul;
+            double jNerr = jNMul * Math.sqrt(Math.pow(r1Err, 2) + Math.pow(1.249 * sigmaErr, 2));
+
+            double j0Mul = 6.0 / (3.0 * d2 + 4.0 * c2);
+            double j0 = j0Mul * (r2 - 0.5 * r1 - 0.454 * sigma);
+            double j0Err = j0Mul * Math.sqrt(Math.pow(r2Err, 2) + Math.pow(0.5 * r1Err, 2) + Math.pow(0.454 * sigmaErr, 2));
+            result[0][0] += j0;
+            result[1][0] = 0.0;
+            result[2][0] += j0Err *j0Err;
+
+            result[0][iField*2+1] = j87H;
+            result[1][iField*2+1] = 0.87 * relaxEq.getWI();
+            result[2][iField*2+1] = j87Herr;
+
+            result[0][iField*2+2] = jN;
+            result[1][iField*2+2] = 0.87 * relaxEq.getWS();
+            result[2][iField*2+2] = jNerr;
+            iField++;
+        }
+        result[0][0] /= nDataValues;
+        result[2][0] = Math.sqrt(result[2][0]);
+        return result;
+    }
+
+    double[][] getJValues() {
+        if (jValues == null) {
+            jValues = calcJ();
+        }
+        return jValues;
+    }
 }
